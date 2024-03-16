2f3301609fe237f069d56047649d82acfa901586
Ritik Kumar <114002245+ritik0027@users.noreply.github.com>
2024-03-16 23:58:53 +0530

    Add files via upload

---
+++ 08reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todo'
+
+function App() {
+
+  return (
+    <>
+    <AddTodo />
+    <Todos />
+    </>
+  )
+}
+
+export default App
